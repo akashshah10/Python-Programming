91393fffa007986f3ff5e7bde21fb4ea1cb5fbc8
akashshah10 <akashmiku.shah@gmail.com>
2025-06-18 18:33:39 +0530

    List and Tuples

---
+++ Python/02_list_methods.py
@@ -0,0 +1,26 @@
+list = ["Mango", "Banana", 5, 345.25, True, "Akash", "Ankit"]
+print(list)
+
+#Using append
+list.append("Stick")
+print(list)
+
+
+list2 = [2 ,25 , 1, 10, 8]
+# #Using sort
+# #It sorts the list in ascending order
+list2.sort()
+print(list2)
+
+# #Using reverse sort
+# #Using this we can sort the list in reverse order
+list2.sort(reverse = True)
+print(list2)
+
+#Using reverse
+#It reverses the order of elements in the list
+list2.reverse()
+print(list2)
+
+#Using insert
+list2.insert(3,200) #Insert 200 such that it,s index is 3
