2f72624bae5d29dcb7611ab69cfc93d5b41f9b79
akashshah10 <akashmiku.shah@gmail.com>
2025-06-28 20:34:07 +0530

    Chapter 5

---
+++ Ch_5_Dictionary and Sets/01_Dictionary.py
@@ -0,0 +1,8 @@
+marks = {
+    "Akash": 80,
+    "Anjali": 97,
+    "Miku": 23
+}
+
+# print(marks, type(markst))
+print(marks["Akash"])
---
+++ Ch_5_Dictionary and Sets/02_Dict_methods.py
@@ -0,0 +1,16 @@
+marks = {
+    "Akash": 80,
+    "Anjali": 97,
+    "Miku": 23,
+    0: "miku"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+# marks.update({"Akash": 88, "Ankit": 100})
+# print(marks)
+
+print(marks.get("Akash2")) #Prints none
+print(marks["Akash2"]) #Returns an error
