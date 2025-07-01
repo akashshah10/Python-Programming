21f7df219739e5ae00355946147f72ae2b86f157
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-07-01 10:34:29 +0530

    Chapter 5

---
+++ Ch_5_Dictionary and Sets/Q1.py
@@ -0,0 +1,11 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+words = {
+    "Dhanyawad" : "Thanks" ,
+    "Shubh Ratri" : "Good Night",
+    "Kela" : "Banana"
+}
+# print(words)
+
+word = input("Enter the word you want the meaning of: ")
+
+print(words[word])
