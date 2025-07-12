b2d229d8f5e4d56de82e2ad2b5410bb812ddb89b
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-07-13 00:50:41 +0530

    Chapter 8

---
+++ Ch_8_Functions/08_functions.py
@@ -0,0 +1,18 @@
+# a = 10
+# b = 54
+# c = 5
+
+# avg = (a+b+c)/3
+# print(avg)
+
+#Finding average using functions
+#Function Definition
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg()#Function call
