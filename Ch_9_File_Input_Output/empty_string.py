ac80f99d987b16ee4f9a409c85067e311dc571b4
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-07-17 22:54:40 +0530

    Update empty_string.py

--- Ch_9_File_Input_Output/empty_string.py
+++ Ch_9_File_Input_Output/empty_string.py
@@ -2,3 +2,11 @@ print(bool(""))        # Output: False
 print(bool("Akash"))   # Output: True
 
 # Since empty strings have no content, Python treats them as False.
+
+a = "Akash"
+if(a == "" or "Akash"): #A non empty string is always treated as True in boolean context. Hence, the output is Ok
+    
+    print("Ok")
+
+else:
+    print("No")
