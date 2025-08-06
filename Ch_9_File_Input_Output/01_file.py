f760a3659a9317413b08f45466cd3bde423bc046
Akash Kumar Shah <208366876+akashshah10@users.noreply.github.com>
2025-08-06 22:27:45 +0530

    Delete Ch_9_File_Input_Output directory

--- Ch_9_File_Input_Output/01_file.py
+++
@@ -1,6 +0,0 @@
-f = open("file.txt")
-data = f.read()
-print(data)
-f.close()
-
-#By default if we open a file it is in read only type
--- Ch_9_File_Input_Output/02_file_write.py
+++
@@ -1,7 +0,0 @@
-string = "Akash is learning steadily, Ok the file is created"
-
-f = open("My_file.txt", "w") #w means write mode 
-
-f.write(string)
-
-f.close()
